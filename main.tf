5697e0d449eb3e02f16a9840fa0485f0758db990
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2026-08-22 09:19:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "name" {
   ami                = "ami-0332d564d76dbd8d6"
   instance_type      = "t3.micro"
   tags = {
-    Name = "manual-server"
+    Name = "gitops-server"
   }
   
 }
