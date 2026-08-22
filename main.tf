b5cea045c0247bf588b4a0f5042f21684131b14f
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2026-08-22 09:18:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "name" {
+  ami                = "ami-0332d564d76dbd8d6"
+  instance_type      = "t3.micro"
+  tags = {
+    Name = "manual-server"
+  }
+  
+}
